31a3398a98237021a06b9c6d0cc5b31109b1455e
Ze Qian <Ze Qian@DESKTOP-0UJST50>
2020-07-22 14:19:48 +0800

    update retrieveAllChromebookTest() in ResourceCentreTest

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -80,8 +80,31 @@ public class ResourceCentreTest {
 	}
 	@Test
 	public void retrieveAllChromebookTest() {
-		//fail("Not yet implemented");
-		// write your code here
+		
+		assertNotNull("Test if there is valid Chromebook arraylist to add to",chromebookList);
+		
+				String allChromeBook= ResourceCentre.retrieveAllChromebook(chromebookList);
+				String testOutput = "";
+				assertEquals("Check that ViewAllChromeBooklist", testOutput, allChromeBook);
+				
+	
+		ResourceCentre.addChromebook(chromebookList, cb1);
+		ResourceCentre.addChromebook(chromebookList, cb2);
+		assertEquals("Test if that Camcorder arraylist size is 2?", 2, chromebookList.size());
+		
+		
+		allChromeBook= ResourceCentre.retrieveAllChromebook(chromebookList);
+
+		testOutput = String.format("%-10s %-30s %-10s\n","CB0011", "My Google Chromebook 1st", "Mac OS");
+		testOutput += String.format("%-10s %-30s %-10s\n","CB0012", "SAMSUNG Chromebook 4+", "Win 10");
+	
+		assertEquals("Check that ViewAllCamcorderlist", testOutput, allChromeBook);
+		
+		
+		
+		
+		
+		
 	}
 
 	@Test
